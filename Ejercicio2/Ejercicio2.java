cf349d6369130e98b2b922bfea21a1cf707f8d27
Facundo <facundo580580@gmail.com>
2021-04-19 20:48:42 -0300

    Correccion de ejercicio 2

--- Ejercicio2/Ejercicio2.java
+++ Ejercicio2/Ejercicio2.java
@@ -10,58 +10,91 @@ public class Ejercicio2 {
 		
 		int cont= 0;
 		
-		List<Integer> lista1;
-		lista1 = new ArrayList<>();
+		List<Integer> lista1 = new ArrayList<>();
 		
-		List<Integer> lista2;
-		lista2 = new ArrayList<>();
+		List<Integer> lista2 = new ArrayList<>();
 		
-		List<Integer> lista3;
-		lista3 = new ArrayList<>();
+		List<Integer> lista3 = new ArrayList<>();
 		
 		Random aleatorio = new Random();
 		int largo1 = 4 + aleatorio.nextInt(10);
 		int largo2 = 9 + aleatorio.nextInt(10);
 		
 		Scanner entrada = new Scanner(System.in);
+		
 		int numero;
 		System.out.println(largo1);
 		System.out.println(largo2);
 		
-		while (cont <= largo1){
+		while (cont < largo1){
 			cont ++;
 			System.out.println("Ingresá un numero entre 50 y 550: ");
-			numero= entrada.nextInt();
+			numero = entrada.nextInt();
 				if ((numero >=50)  && (numero <= 550)) {
 					lista1.add(numero);
-				}
-				else { System.out.println("El número no esta en el rango esperado.");}
+				} else { 
+					System.out.println("El número no esta en el rango esperado.");
+					}
 			}
+
 			System.out.println(lista1);
 			
-				while (cont <= largo2){  //Creo otro while porque sino los elementos coinciden si o si.
-			cont ++;
+			cont = 0;
+			
+				while (cont < largo2){  //Creo otro while porque sino los elementos coinciden si o si.
+			cont++;
 			System.out.print("Ingresá un numero entre 50 y 550: ");
 			numero= entrada.nextInt();
 				if ((numero >=50)  && (numero <= 550)) {
 					lista2.add(numero);
-				}
-				else { System.out.println("El número no esta en el rango esperado.");}
+				} else { 
+					System.out.println("El número no esta en el rango esperado.");
+					}
 			}
-			System.out.print(lista2);
+			System.out.println(lista2);
 			
-			for (int i=0; i <= cont; i++){
-				 if (lista1.get(i) == lista2.get(i)){
+			for (int i : lista1){
+				 if (!lista2.contains(i)){
 					 lista3.add(i);
 					 }
 				}
-
-			
-			System.out.print(lista3);
-			
 	
-		
-		
+			System.out.println(lista3);	
 	}
 }
+
+
+/*
+boolean estaEnLaLista;
+
+for(int numero: lista1){
+	
+	estaEnLaLista = false;
+	
+	for(int num: lista2){
+		if (numero == num){
+			estaEnLaLista = true;
+			break;
+			}
+				}
+				
+			if (!estaEnLaLista){
+				lista3.add(numero);
+				}
+	}
+
+
+*/
+
+
+/*
+ * for(int numero: lista1){
+ * if (!lista2.contains(numero)) lista3.add(numero);
+ * }
+ * 
+ * 
+ * 
+ * 
+ * */
+
 
